5534a0e617867d31068065f0fbad83fa1c830299
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-19 23:43:06 +0530

    done

--- instagram/index.php
+++ instagram/index.php
@@ -1,5 +1,5 @@
 <?php
 
-echo '<a href="https://api.instagram.com/oauth/authorize/?client_id=faf4debe2fe74963833bd88249c3c9b0&redirect_uri=http://www.amituslab.com/instagram/redirect.php&response_type=code">Authenticate</a>';
+echo '<a href="https://api.instagram.com/oauth/authorize/?client_id=faf4debe2fe74963833bd88249c3c9b0&redirect_uri=http://apis1.herokuapp.com/instagram/redirect.php&response_type=code">Authenticate</a>';
 
 ?>
