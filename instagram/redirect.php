15b7ec493efe2911e0f76bfdcb61fab90e8bde93
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-20 00:26:21 +0530

    done

--- instagram/redirect.php
+++ instagram/redirect.php
@@ -6,7 +6,7 @@ $code = "";
 $url = "";
 $client_id = 'faf4debe2fe74963833bd88249c3c9b0';
 $client_secret = '5e0e72148ad64e61aeb5e8db630a7646';
-$redirect_uri = 'http://apis1.herokuapp.com/redirect.php';
+$redirect_uri = 'http://apis1.herokuapp.com/instagram/redirect.php';
 
 echo $_SESSION['access_token'];
 
