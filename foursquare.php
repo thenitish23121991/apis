2d590e6b5d5daf49eaf6c2447ef46725a532c8a3
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-19 23:32:53 +0530

    done

--- foursquare.php
+++ foursquare.php
@@ -13,7 +13,7 @@ require_once('db.php');
 <body>
 
 <div class="container">
-<a href="https://foursquare.com/oauth2/authenticate?client_id=VDJY1PO1SNWXQ0LBFWFYSMG4BIUPB53JBCG4ZOFTEAWZC32K&response_type=code&redirect_uri=http://www.collegeink.in/foursquare/redirect.php">Authenticate</a>
+<a href="https://foursquare.com/oauth2/authenticate?client_id=K2FDDKAO3KNK5Q0V1QCAPR2K02L0SEPOYO4ZMWS4AU0KM2M3&response_type=code&redirect_uri=http://apis1.herokuapp.com/foursquare/redirect.php">Authenticate</a>
 </div>
 
 
