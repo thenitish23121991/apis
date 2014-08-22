8f00f54ca77e6d06c350b1ac15729e9dc1c9ac5d
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-23 04:34:22 +0530

    done

---
+++ twitter_feed.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+require_once('twitteroauth.php');
+require_once('twitter_config.php');
+
+$connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
+$content = $connection->get('statuses/user_timeline',array('screen_name' => $screen_name,'exclude_replies' => 'true','include_rts' => 'false','count' => 30));
+
+foreach($content as $c){
+print_r($c).'<br/><br/>';
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+
+<body>
+
+<div class="container">
+
+</div>
+
+</body>
+</html>
