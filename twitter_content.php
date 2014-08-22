90bce1b539640c22e9765195334a9c8224c2e9ff
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-23 04:11:33 +0530

    done

--- twitter_content.php
+++ twitter_content.php
@@ -3,6 +3,7 @@ session_start();
 require_once('twitteroauth.php');
 require_once('twitter_config.php');
 
+print_r($_SESSION);
 
 $connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['request_token'],$_SESSION['request_token_secret']);
 $content = $connection->get('account/verify_credentials');
