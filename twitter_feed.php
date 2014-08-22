bf2573689ce79e395ca37c823e0c0720d9b6d55f
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-23 04:41:14 +0530

    done

--- twitter_feed.php
+++ twitter_feed.php
@@ -7,13 +7,10 @@ $connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_t
 $content = $connection->get('statuses/user_timeline',array('screen_name' => $screen_name,'exclude_replies' => 'true','include_rts' => 'false','count' => 30));
 
 foreach($content as $c){
-echo '<div><div>'.
-$c['user']['screen_name']
-.'</div>
-<div>'.
+echo '<div>'.
 $c['text']
 .'</div>
-</div><br/>';
+<br/>';
 }
 
 ?>
