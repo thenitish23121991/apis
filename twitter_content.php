d82caebfd9c3887895d823f8406bb0f63b39ff18
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-23 04:30:17 +0530

    done

--- twitter_content.php
+++ twitter_content.php
@@ -6,6 +6,22 @@ require_once('twitter_config.php');
 $connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
 $content = $connection->get('account/verify_credentials');
 
-print_r($content);
+
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+
+<body>
+
+<div class="container">
+
+</div>
+
+</body>
+</html>
