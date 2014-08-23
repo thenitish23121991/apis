24138bb1ac16319b0121b5ee0983fc9b2f71e865
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-23 16:59:07 +0530

    done

---
+++ twitter_info.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require_once('twitteroauth.php');
+require_once('twitter_config.php');
+
+$connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
+$content = $connection->get('account/verify_credentials',array('screen_name' => $screen_name,'exclude_replies' => 'true','include_rts' => 'false','count' => 30));
+
+print_r($content);
+/*
+echo 'Screen Name: '.$content->screen_name.'<br/>';
+echo 'Name: '.$content->name.'<br/>';
+*/
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<title></title>
+</head>
+
+
+<body>
+
+<div class="container">
+
+</div>
+
+</body>
+</html>
