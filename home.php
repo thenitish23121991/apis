c2b6925a2f0c358d9a48c39651dfaacfab76dcae
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-24 04:04:08 +0530

    done

--- home.php
+++ home.php
@@ -8,7 +8,7 @@
 
 <a href="index.php">Facebook API</a>
 <a href="twitter_login.php">Twitter API</a>
-<a href="instagram/instagram.php">Instagram API</a>
+<a href="instagram/index.php">Instagram API</a>
 <a href="foursquare/f1.php">Foursquare API</a>
 
 </body>
