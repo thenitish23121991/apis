23e47742279bf613ce574ec80933159f1de06284
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-24 04:20:28 +0530

    done

--- foursquare/f1.php
+++ foursquare/f1.php
@@ -9,7 +9,7 @@
 <body>
 
 <div class="container">
-<a href="https://foursquare.com/oauth2/authenticate?client_id=K2FDDKAO3KNK5Q0V1QCAPR2K02L0SEPOYO4ZMWS4AU0KM2M3&response_type=code&redirect_uri=http://apis1.herokuapp.com/foursquare/redirect.php">Authenticate</a>
+<a href="https://foursquare.com/oauth2/authenticate?client_id=VDJY1PO1SNWXQ0LBFWFYSMG4BIUPB53JBCG4ZOFTEAWZC32K&response_type=code&redirect_uri=http://apis1.herokuapp.com/foursquare/redirect.php">Authenticate</a>
 </div>
 
 
