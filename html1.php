c298e5b15dd95988ce9984414dcfcb67562ce0a2
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 21:44:05 +0530

    done

--- html1.php
+++ html1.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors','1');
 require_once('simple_html_dom.php');
 
-$html = file_get_html('www.google.com');
+$html = file_get_html('http://www.google.com');
 echo $html; 
 
 ?>
