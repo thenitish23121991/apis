dad95c58773453c7fc1d8cf183f834ab482209ab
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 21:59:12 +0530

    done

--- index.php
+++ index.php
@@ -15,11 +15,20 @@ $_SESSION['url_name'] = $_POST['url_name'];
 
 if(isset($_SESSION['url_name']) && !empty($_SESSION['url_name'])){
 
+/*
 $context = stream_context_create();
 stream_context_set_params($context, array('user_agent' => 'UserAgent/1.0');
 $url = 'http://www.facebook.com/'.$_SESSION['url_name'].'/friends';
 $html = file_get_html($url,0,$context);
-echo $html;
+
+*/
+$url = "oooff.com";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+$curl_scraped_page = curl_exec($ch);
+curl_close($ch);
+echo $curl_scraped_page;
 
 
 }
