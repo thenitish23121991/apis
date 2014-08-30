31d775e569685f9b8096fe441159aa9c4dab6e75
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 22:31:31 +0530

    done

--- index.php
+++ index.php
@@ -13,14 +13,6 @@ $_SESSION['url_name'] = $_POST['url_name'];
 }
 
 
-$url = "www.tympanus.net";
-$ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-$curl_scraped_page = curl_exec($ch);
-curl_close($ch);
-var_dump($curl_scraped_page);
-
 if(isset($_SESSION['url_name']) && !empty($_SESSION['url_name'])){
 
 /*
