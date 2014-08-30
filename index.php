a6b2dfbd49d0211d3b8bcb1bf00908067858742e
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 22:04:54 +0530

    done

--- index.php
+++ index.php
@@ -19,8 +19,7 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 $curl_scraped_page = curl_exec($ch);
 curl_close($ch);
-echo $curl_scraped_page;
-echo 'yes';
+var_dump($curl_scraped_page);
 
 if(isset($_SESSION['url_name']) && !empty($_SESSION['url_name'])){
 
