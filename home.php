cbb1d2ec979e64af479df0b3c0388d3b0ad93536
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 22:32:32 +0530

    done

--- home.php
+++ home.php
@@ -6,10 +6,10 @@
 
 <body>
 
-<a href="index.php">Facebook API</a>
-<a href="twitter_login.php">Twitter API</a>
-<a href="instagram/index.php">Instagram API</a>
-<a href="foursquare/f1.php">Foursquare API</a>
+<a href="index.php">Login with Facebook</a>
+<a href="twitter_login.php">Login with Twitter</a>
+<a href="instagram/instagram.php">Login with Instagram</a>
+<a href="foursquare/f1.php">Login with Foursquare</a>
 
 </body>
 </html>
