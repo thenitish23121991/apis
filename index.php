d5f47801193ad2aca20bccbccf421e1f7b5564da
Nitish Hooda <thenitish23121991@yahoo.in>
2014-08-30 22:00:25 +0530

    done

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ $url = 'http://www.facebook.com/'.$_SESSION['url_name'].'/friends';
 $html = file_get_html($url,0,$context);
 
 */
-$url = "oooff.com";
+$url = "www.google.com";
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
