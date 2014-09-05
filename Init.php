fb6057ab571885e5f598ce272135eaec55d2cc80
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-06 04:20:37 +0530

    done

--- Init.php
+++ Init.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors','1');
 require_once('db_file.php');
 require_once('User.php');
-require_once('Feed.php');
+
 
 $user = new User($con);
 $feed = new Feed($con);
