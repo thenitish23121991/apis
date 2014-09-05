b5faf7ff79313880b6e2b3071829fcb77bbd7550
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-06 04:19:22 +0530

    done

--- db_file.php
+++ db_file.php
@@ -1,5 +1,5 @@
 <?php
 
-$con = new PDO('mysql:host=localhost;dbname=amitusla_apis;charset=utf8','amitusla_apis','thankyousachin13');
+$con = new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_665a3580d60dca3;charset=utf8','b584fd89c37150','2ad3c6c0');
 
 ?>
