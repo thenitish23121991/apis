c9f2836cd7e78e101f7bf21ec1bf3c476ede92dd
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-06 03:45:38 +0530

    done

--- User.php
+++ User.php
@@ -10,6 +10,8 @@ $this->db = $db;
 
 public function add_user($first_name,$last_name,$f_id='',$i_id='',$t_id='',$fo_id=''){
 $user_arr = array();
+$user_result = $this->is_user_present($first_name,$last_name);
+if($user_result['result'] == 'no'){
 try{
 $query = "INSERT INTO `users`(first_name,last_name,f_id,i_id,t_id,fo_id) VALUES(?,?,?,?,?,?)";
 $sql = $this->db->prepare($query);
@@ -21,10 +23,12 @@ $user_arr['result'] = "success";
 $user_arr['result'] = "nosuccess";
 }
 }
+
 catch(PDOException $e){
 $e->getMessage();
 }
 return $user_arr;
+}
 }
 
 
@@ -68,7 +72,24 @@ $e->getMessage();
 return $user_arr;
 }
 
-
+public function is_user_present($first_name,$last_name){
+$user_arr = array();
+try{
+$query = "SELECT * FROM `friends` WHERE first_name=? AND last_name=?";
+$sql = $this->db->prepare($query);
+$sql->execute(array($first_name,$last_name));
+$sqlnum = $sql->rowCount();
+if($sqlnum > 0){
+$user_arr['result'] = "success";
+}else{
+$user_arr['result'] = "nosuccess";
+}
+}
+catch(PDOException $e){
+$e->getMessage();
+}
+return $user_arr;
+}
 
 
 
