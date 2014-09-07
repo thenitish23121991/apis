fb22f3b28e61b03e4137153cbea7849f129bb1c3
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 22:18:26 +0530

    done

--- foursquare/basic.php
+++ foursquare/basic.php
@@ -7,7 +7,7 @@ $access_token = "";
 $action_ch = curl_init();
 curl_setopt($action_ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($action_ch, CURLOPT_RETURNTRANSFER, true);
-$action_url = 'https://api.foursquare.com/v2/users/self?oauth_token='.$_SESSION['foursquare_access_token'].'&v=20130815&limit=100';
+$action_url = 'https://api.foursquare.com/v2/users/self?oauth_token='.$_SESSION['foursquare_access_token'].'&v=20130815';
 curl_setopt($action_ch, CURLOPT_URL,$action_url);
 $action_result=curl_exec($action_ch);
 curl_close($action_ch);
