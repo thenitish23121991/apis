b3863e50d9f182f0e44eca925cd4650e6229b46b
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 20:54:30 +0530

    done

--- foursquare/get_my_checkins.php
+++ foursquare/get_my_checkins.php
@@ -3,8 +3,6 @@ session_start();
 error_reporting(E_ALL);
 ini_set('display_errors','1');
 
-echo $_SESSION['foursquare_access_token'];
-
 $action_ch = curl_init();
 curl_setopt($action_ch, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($action_ch, CURLOPT_RETURNTRANSFER, true);
@@ -12,7 +10,6 @@ $action_url = 'https://api.foursquare.com/v2/users/self/checkins?oauth_token='.$
 curl_setopt($action_ch, CURLOPT_URL,$action_url);
 $action_result=curl_exec($action_ch);
 curl_close($action_ch);
-print_r($action_result);
 $action_result_json = json_decode($action_result);
 $action_checkins = $action_result_json->response->checkins->items;
 var_dump($action_result_json);
