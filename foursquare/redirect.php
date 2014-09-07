4a2053332e8aeda7c4d3644c80b6ef76ae4a5937
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 23:04:54 +0530

    done

--- foursquare/redirect.php
+++ foursquare/redirect.php
@@ -26,6 +26,26 @@ $result_json = json_decode($result);
 $access_token = $result_json->access_token;
 $_SESSION['foursquare_access_token'] = $access_token;
 
+
+
+$action_ch = curl_init();
+curl_setopt($action_ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($action_ch, CURLOPT_RETURNTRANSFER, true);
+$action_url = 'https://api.foursquare.com/v2/users/self?oauth_token='.$_SESSION['foursquare_access_token'].'&v=20130815';
+curl_setopt($action_ch, CURLOPT_URL,$action_url);
+$action_result=curl_exec($action_ch);
+curl_close($action_ch);
+$action_result_json = json_decode($action_result);
+//var_dump($action_result_json);
+
+$first_name = $action_result_json->response->user->firstName;
+$last_name = $action_result_json->response->user->lastName;
+$fo_id = $action_result_json->response->user->id;
+
+
+$user->add_user($first_name,$last_name,'foursquare','','','',$fo_id);
+
+
 /*
 
 if(isset($_GET['action'])){
@@ -55,9 +75,7 @@ $action_result_json = json_decode($action_result);
 print_r($action_result_json);
 */
 
-if(!isset($_SESSION['user'])){
-header('Location: basic.php');
-}
+
 
 ?>
 
