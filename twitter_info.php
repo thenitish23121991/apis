e0b15df7ad656f51ec9ae582179ef8d8510cfd11
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 19:49:23 +0530

    done

--- twitter_info.php
+++ twitter_info.php
@@ -2,13 +2,22 @@
 session_start();
 require_once('twitteroauth.php');
 require_once('twitter_config.php');
+require_once('Init.php');
 
 $connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
 $content = $connection->get('account/verify_credentials',array('screen_name' => $screen_name,'exclude_replies' => 'true','include_rts' => 'false','count' => 30));
 
 echo 'Screen Name: '.$content->screen_name.'<br/>';
 echo 'Name: '.$content->name.'<br/>';
-echo ''
+echo 'ID: '.$content->id.'<br/>';
+
+$name_arr = explode(" ",$content->name);
+$first_name = $name_arr[0];
+$last_name = $name_arr[1];
+$t_id = $content->id;
+
+$user->add_user($first_name,$last_name,'','',$t_id,'');
+
 
 ?>
 
