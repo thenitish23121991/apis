565ce8a8e186321e978ad1dc379ce8954aea763d
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 20:49:21 +0530

    done

--- foursquare/redirect.php
+++ foursquare/redirect.php
@@ -9,6 +9,7 @@ $code = $_GET['code'];
 
 
 $_SESSION['code'] = $code;
+echo $_SESSION['code'];
 
 
 $login_url = 'https://foursquare.com/oauth2/access_token?client_id=K2FDDKAO3KNK5Q0V1QCAPR2K02L0SEPOYO4ZMWS4AU0KM2M3&client_secret=AUHH5G3YSK0W2KT2ZFYLZ2KYBG52Y1BVISEY0RVF52SFXRRE&grant_type=authorization_code&redirect_uri=http://apis1.herokuapp.com/foursquare/redirect.php&code='.$_SESSION['code'];
