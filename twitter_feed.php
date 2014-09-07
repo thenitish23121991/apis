e5d61fe54b0211dae963b73d2962ec17097f9be5
Nitish Hooda <thenitish23121991@yahoo.in>
2014-09-07 20:11:30 +0530

    done

--- twitter_feed.php
+++ twitter_feed.php
@@ -2,11 +2,13 @@
 session_start();
 require_once('twitteroauth.php');
 require_once('twitter_config.php');
+require_once('Init.php');
 
 $connection = new TwitterOAuth($consumer_key,$consumer_secret,$_SESSION['oauth_token'],$_SESSION['oauth_token_secret']);
-$content = $connection->get('statuses/user_timeline',array('screen_name' => $screen_name,'exclude_replies' => 'true','include_rts' => 'false','count' => 30));
+$content = $connection->get('statuses/user_timeline',array('exclude_replies' => 'true','include_rts' => 'false','count' => 30));
 
 foreach($content as $c){
+print_r($c).'<br/><br/><br/>';
 echo '<div><div>'.
 $c->user->screen_name
 .'</div>
